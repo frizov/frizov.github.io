53963708e37c143b691cfec84f3213589f37519b
Filip <filiprizov@gmail.com>
2024-09-12 13:08:21 +0200

    commit

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
 
-function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
+// import { without } from "lodash";
+// console.log(without);
+var _ = require('lodash');
 
-	css.textContent = body.style.background + ";";
-}
+var array = [1,2,3,4,5,6,7,8];
 
-color1.addEventListener("input", setGradient);
-
-color2.addEventListener("input", setGradient);
+console.log('answer:' , _.without(array, 3));
